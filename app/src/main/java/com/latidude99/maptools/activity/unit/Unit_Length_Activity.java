5e6189d84e10a06fa3f28456d87f77cd67377555
latidudue99 <latidude99@gmail.com>
2020-01-19 19:57:32 +0000

    Create Length Converter option in Feature 3

--- app/src/main/java/com/latidude99/maptools/activity/unit/Unit_Length_Activity.java
+++ app/src/main/java/com/latidude99/maptools/activity/unit/Unit_Length_Activity.java
@@ -2,19 +2,287 @@ package com.latidude99.maptools.activity.unit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.latidude99.maptools.R;
+import com.latidude99.maptools.model.scale.InsufficientParameterException;
+import com.latidude99.maptools.model.scale.Scale;
+import com.latidude99.maptools.model.unit.LengthEntry;
+
+import java.text.DecimalFormat;
+import java.util.Locale;
 
 public class Unit_Length_Activity extends AppCompatActivity {
+    Locale locale = Locale.getDefault();
+    EditText textInputLength;
+    Spinner spinnerInputLengthUnits;
+    TextView textLengthUM;
+    TextView textLengthMM;
+    TextView textLengthCM;
+    TextView textLengthIN;
+    TextView textLengthFT;
+    TextView textLengthYD;
+    TextView textLengthM;
+    TextView textLengthFTM;
+    TextView textLengthKM;
+    TextView textLengthMI;
+    TextView textLengthNM;
+
+    private String ERROR_INPUT_INT;
+    private String ERROR_INPUT_EMPTY;
+    private String ERROR_INPUT_0;
+    private String ERROR_INPUT_1;
+    private String ERROR_INPUT_TOO_BIG;
+    private String ERROR_UNIT_CONVERSION;
+
+    LengthEntry lengthEntry;
+
+    // used to store info/calculation when orientation changes
+    private boolean inputCleared;
+    private boolean converted;
+    private double storedLengthUM;  //  length in micrometres
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unit_length);
 
-        TextView textLengthUM = (TextView) findViewById(R.id.converted_length_um);
-        textLengthUM.setText("1342222222222222");
+        ERROR_INPUT_INT = getString(R.string.error_input_int);
+        ERROR_INPUT_EMPTY = getString(R.string.error_input_empty);
+        ERROR_INPUT_0 = getString(R.string.error_input_zero);
+        ERROR_INPUT_1 = getString(R.string.error_input_one);
+        ERROR_INPUT_TOO_BIG = getString(R.string.error_input_too_big);
+        ERROR_UNIT_CONVERSION = getString(R.string.error_unit_conversion);
+
+        textInputLength = (EditText) findViewById(R.id.unit_length_value);
+        spinnerInputLengthUnits = (Spinner) findViewById(R.id.unit_length_units);
+        textLengthUM = (TextView) findViewById(R.id.converted_length_um);
+        textLengthMM = (TextView) findViewById(R.id.converted_length_mm);
+        textLengthCM = (TextView) findViewById(R.id.converted_length_cm);
+        textLengthIN = (TextView) findViewById(R.id.converted_length_in);
+        textLengthFT = (TextView) findViewById(R.id.converted_length_ft);
+        textLengthYD = (TextView) findViewById(R.id.converted_length_yd);
+        textLengthM = (TextView) findViewById(R.id.converted_length_m);
+        textLengthFTM = (TextView) findViewById(R.id.converted_length_ftm);
+        textLengthKM = (TextView) findViewById(R.id.converted_length_km);
+        textLengthMI = (TextView) findViewById(R.id.converted_length_statue_mile);
+        textLengthNM = (TextView) findViewById(R.id.converted_length_nautical_mile);
+
+        Button btnConvert = (Button) findViewById(R.id.btn_convert);
+        btnConvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String stringInput = textInputLength.getText().toString().trim();
+                processInputAndConvertLength(view);
+            }
+        });
+
+        // 1st click clears converted length fields
+        // 2nd click clears entered value
+        Button btnClear = (Button) findViewById(R.id.btn_clear);
+        btnClear.setOnClickListener(new View.OnClickListener() {;
+            @Override
+            public void onClick(View view) {
+                if(inputCleared)
+                    textInputLength.setText("");
+                else{
+                    clearTextFields();
+                    showKeyboard(view);
+                }
+            }
+        });
+
+
+
     }
+
+    // ---------------------- end onCreate() and similar inherited methods ----------------------
+
+    private void processInputAndConvertLength(View view){
+        String stringInputLength = textInputLength.getText().toString();
+
+        System.out.println("stringInputLength: " + stringInputLength);
+
+        if("".equals(stringInputLength)){
+            Toast.makeText(this, ERROR_INPUT_EMPTY,Toast.LENGTH_SHORT).show();
+        }else if ("0".equals(stringInputLength)){
+            Toast.makeText(this, ERROR_INPUT_0,Toast.LENGTH_SHORT).show();
+        }else {
+            int positionInputLengthUnit = spinnerInputLengthUnits.getSelectedItemPosition();
+
+            convertLengthAndDisplay(stringInputLength, positionInputLengthUnit);
+            hideKeyboard(view);
+            converted = true;
+            inputCleared = false;
+        }
+    }
+
+    private void convertLengthAndDisplay(String stringInput, int unit){
+        double value = convertStringToDoubleInput(stringInput);
+        try{
+            switch(unit){
+                case 0: // um
+                    lengthEntry = new LengthEntry.Builder()
+                            .setMicrometresAndConvertAll(value)
+                            .build();
+                    break;
+                case 1: // mm
+                    lengthEntry = new LengthEntry.Builder()
+                            .setMillimetresAndConvertAll(value)
+                            .build();
+                    break;
+                case 2: // cm
+                    lengthEntry = new LengthEntry.Builder()
+                            .setCentimetresAndConvertAll(value)
+                            .build();
+                    break;
+                case 3: // in
+                    lengthEntry = new LengthEntry.Builder()
+                            .setInchesAndConvertAll(value)
+                            .build();
+                    break;
+                case 4: // ft
+                    lengthEntry = new LengthEntry.Builder()
+                            .setFeetAndConvertAll(value)
+                            .build();
+                    break;
+                case 5: // yd
+                    lengthEntry = new LengthEntry.Builder()
+                            .setYardsAndConvertAll(value)
+                            .build();
+                    break;
+                case 6: // m
+                    lengthEntry = new LengthEntry.Builder()
+                            .setMetresAndConvertAll(value)
+                            .build();
+                    break;
+                case 7: // ftm
+                    lengthEntry = new LengthEntry.Builder()
+                            .setFathomsAndConvertAll(value)
+                            .build();
+                    break;
+                case 8: // km
+                    lengthEntry = new LengthEntry.Builder()
+                            .setKilometresAndConvertAll(value)
+                            .build();
+                    break;
+                case 9: // mi
+                    lengthEntry = new LengthEntry.Builder()
+                            .setMilesAndConvertAll(value)
+                            .build();
+                    break;
+                case 10: // nm
+                    lengthEntry = new LengthEntry.Builder()
+                            .setNauticalMilesAndConvertAll(value)
+                            .build();
+                    break;
+            }
+            if(lengthEntry != null)
+                displayConvertedValues(lengthEntry);
+            else
+                clearTextFields();
+        }catch (InsufficientParameterException e){
+            e.printStackTrace();
+            Toast.makeText(this, ERROR_UNIT_CONVERSION, Toast.LENGTH_SHORT);
+        }
+    }
+
+    private void displayConvertedValues(LengthEntry lengthEntry){
+        textLengthUM.setText(new DecimalFormat("#,###").format(lengthEntry.getUm()));
+        textLengthMM.setText(new DecimalFormat("#,###.00").format(lengthEntry.getMm()));
+        textLengthCM.setText(new DecimalFormat("#,###.00").format(lengthEntry.getCm()));
+        textLengthIN.setText(new DecimalFormat("#,###.00").format(lengthEntry.getIn()));
+        textLengthFT.setText(new DecimalFormat("#,###.00").format(lengthEntry.getFt()));
+        textLengthYD.setText(new DecimalFormat("#,###.00").format(lengthEntry.getYd()));
+        textLengthM.setText(new DecimalFormat("#,###.00").format(lengthEntry.getM()));
+        textLengthFTM.setText(new DecimalFormat("#,###.00").format(lengthEntry.getFtm()));
+        textLengthKM.setText(new DecimalFormat("#,###.00").format(lengthEntry.getKm()));
+        textLengthMI.setText(new DecimalFormat("#,###.00").format(lengthEntry.getMi()));
+        textLengthNM.setText(new DecimalFormat("#,###.00").format(lengthEntry.getNm()));
+    }
+
+    private double convertStringToDoubleInput(String input){
+        double doubleInput = 0;
+        if(input != null){
+            try{
+                doubleInput = Double.parseDouble(input);
+            }catch(NumberFormatException e){
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(), ERROR_INPUT_INT, Toast.LENGTH_SHORT).show();
+            }
+            if(doubleInput <= 0)
+                Toast.makeText(this, ERROR_INPUT_0, Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, ERROR_INPUT_EMPTY, Toast.LENGTH_SHORT).show();
+        }
+        return doubleInput;
+    }
+
+    private void clearTextFields(){
+        textLengthUM.setText("");
+        textLengthMM.setText("");
+        textLengthCM.setText("");
+        textLengthIN.setText("");
+        textLengthFT.setText("");
+        textLengthYD.setText("");
+        textLengthM.setText("");
+        textLengthFTM.setText("");
+        textLengthKM.setText("");
+        textLengthMI.setText("");
+        textLengthNM.setText("");
+        lengthEntry = null;
+        inputCleared = true;
+    }
+
+    private void hideKeyboard(View view) {
+        // Check if no view has focus:
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(),0);
+        }
+        view.clearFocus();
+    }
+
+    private void showKeyboard(View view) {
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT,0);
+        }
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/latidude99/maptools/model/unit/LengthEntry.java
+++ app/src/main/java/com/latidude99/maptools/model/unit/LengthEntry.java
@@ -6,21 +6,23 @@ import java.util.Locale;
 
 public class LengthEntry {
     Locale locale = Locale.getDefault();
-    private double um;
-    private double mm;
-    private double cm;
-    private double ft;
-    private double yd;
-    private double m;
-    private double ftm;
-    private double km;
-    private double mi;
-    private double nm;
+    private double um; // micrometre
+    private double mm; // millimetre
+    private double cm; // centimetre
+    private double in; // inch
+    private double ft; // foot
+    private double yd; // yard
+    private double m; // metre
+    private double ftm; // fathom
+    private double km; // kilometre
+    private double mi; // statue mile, UK
+    private double nm; // nautical mile, inernational
 
-    private LengthEntry(LengthBuilder builder){
+    private LengthEntry(Builder builder){
         this.um = builder.um;
         this.mm = builder.mm;
         this.cm = builder.cm;
+        this.in = builder.in;
         this.ft = builder.ft;
         this.yd = builder.yd;
         this.m = builder.m;
@@ -38,6 +40,9 @@ public class LengthEntry {
     }
     public double getCm() {
         return cm;
+    }
+    public double getIn() {
+        return in;
     }
     public double getFt() {
         return ft;
@@ -61,10 +66,11 @@ public class LengthEntry {
         return nm;
     }
 
-    public static class LengthBuilder{
+    public static class Builder {
         private double um;
         private double mm;
         private double cm;
+        private double in;
         private double ft;
         private double yd;
         private double m;
@@ -73,138 +79,330 @@ public class LengthEntry {
         private double mi;
         private double nm;
 
-        public LengthBuilder(){
+        public Builder() {
         }
 
-        public LengthBuilder setMicrometresAndConvertAll(double um){
+        public Builder setMicrometresAndConvertAll(double um) {
             this.um = um;
             convertAllFromUM(um);
             return this;
         }
 
-        public LengthBuilder setMillimetresAndConvertAll(double mm){
+        public Builder setMillimetresAndConvertAll(double mm) {
             this.mm = mm;
             convertAllFromMM(mm);
             return this;
         }
 
-        public LengthBuilder setCentimetresAndConvertAll(double cm){
+        public Builder setCentimetresAndConvertAll(double cm) {
             this.cm = cm;
             convertAllFromCM(cm);
             return this;
         }
 
-        public LengthBuilder setFeetAndConvertAll(double ft){
+        public Builder setInchesAndConvertAll(double cm) {
+            this.cm = cm;
+            convertAllFromCM(cm);
+            return this;
+        }
+
+        public Builder setFeetAndConvertAll(double ft) {
             this.ft = ft;
             convertAllFromFT(ft);
             return this;
         }
 
-        public LengthBuilder setYardsAndConvertAll(double yd){
+        public Builder setYardsAndConvertAll(double yd) {
             this.yd = yd;
             convertAllFromYD(yd);
             return this;
         }
 
-        public LengthBuilder setMetresAndConvertAll(double m){
+        public Builder setMetresAndConvertAll(double m) {
             this.m = m;
             convertAllFromM(m);
             return this;
         }
 
-        public LengthBuilder setFathomsAndConvertAll(double ftm){
+        public Builder setFathomsAndConvertAll(double ftm) {
             this.ftm = ftm;
             convertAllFromFTM(ftm);
             return this;
         }
 
-        public LengthBuilder setKilometresAndConvertAll(double km){
+        public Builder setKilometresAndConvertAll(double km) {
             this.km = km;
             convertAllFromKM(km);
             return this;
         }
 
-        public LengthBuilder setMilesAndConvertAll(double mi){
+        public Builder setMilesAndConvertAll(double mi) {
             this.mi = mi;
             convertAllFromMI(mi);
             return this;
         }
 
-        public LengthBuilder setNauticalMilesAndConvertAll(double nm){
+        public Builder setNauticalMilesAndConvertAll(double nm) {
             this.nm = nm;
             convertAllFromNM(nm);
             return this;
         }
 
-        private boolean convertAllFromUM(double input){
-            this.um = input;
-            this.mm = input *10D;
-            this.cm = input * 100D;
-            this.ft = input;
-            this.yd = input;
-            this.m = input * 10_000D;
-            this.ftm = input;
-            this.km = input * 10_000_000;
-            this.mi = input;
-            this.nm = input;
-        }
-    }
-
-            this.um = input;
-            this.mm = input;
-            this.cm = input;
-            this.ft = input;
-            this.yd = input;
-            this.m = input;
-            this.ftm = input;
-            this.km = input;
-            this.mi = input;
-            this.nm = input;
-
-        private void convertAllFromInchtomile(double inches){
-            this.fractional = (int) (63360 / inches);
-            this.inToMile = inches;
-            this.mileToIn = 1 / inches;
-            this.cmToKm =  100000 / (63360 / inches);
-            this.kmToCm = (63360 / inches) / 100000;
+        private boolean convertAllFromUM(double input) {
+            try {
+                this.um = input;
+                this.mm = input / 1_000;
+                this.cm = input / 10_000;
+                this.in = input / 25_400;
+                this.ft = input / 304_800;
+                this.yd = input / 914_400;
+                this.m = input / 1_000_000;
+                this.ftm = input / 1_828_800;
+                this.km = input / 1_000_000_000;
+                this.mi = input / 1_609_344_000;
+                this.nm = input / 1_852_000_000;
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                return false;
+            }
         }
 
-        private void convertAllFromMiletoinch(double miles){
-            this.fractional = (int) (miles * 63360);
-            this.inToMile = 1 / miles;
-            this.mileToIn = miles;
-            this.cmToKm =  100000 / (miles * 63360);
-            this.kmToCm = (miles * 63360) / 100000;
+
+        private boolean convertAllFromMM(double input) {
+            try {
+                this.um = input * 1000;
+                this.mm = input;
+                this.cm = input / 10;
+                this.in = input / 25.4;
+                this.ft = input / 304.8;
+                this.yd = input / 914.4;
+                this.m = input / 1_000;
+                this.ftm = input / 1_828.8;
+                this.km = input / 1_000_000;
+                this.mi = input / 1_609_344;
+                this.nm = input / 1_852_000;
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                return false;
+            }
         }
 
-        private void convertAllFromCmtokm(double cm){
-            this.fractional = (int) (100000 / cm);
-            this.inToMile = 63360 / (100000 / cm);
-            this.mileToIn = (100000 / cm) / 63360;
-            this.cmToKm = cm;
-            this.kmToCm = 1 /cm;
+        private boolean convertAllFromCM(double input) {
+            try {
+                this.um = input * 10_000;
+                this.mm = input * 10;
+                this.cm = input;
+                this.in = input / 25.4;
+                this.ft = input / 30.48;
+                this.yd = input / 91.44;
+                this.m = input /100;
+                this.ftm = input / 182.88;
+                this.km = input / 100_000;
+                this.mi = input / 160_934.4;
+                this.nm = input / 185_200;
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                return false;
+            }
         }
 
-        private void convertAllFromKmtocm(double km){
-            this.fractional = (int) (100000 * km);
-            this.inToMile = 63360 / (100000 * km);
-            this.mileToIn = (100000 * km) / 63360;
-            this.cmToKm = 1 /km ;
-            this.kmToCm = km;
+        private boolean convertAllFromIN(double input) {
+            try {
+                this.um = input * 25_400;
+                this.mm = input * 25.4;
+                this.cm = input * 2.54;
+                this.in = input;
+                this.ft = input / 12;
+                this.yd = input / 36;
+                this.m = input / 39.370079;
+                this.ftm = input / 72;
+                this.km = input / 39_370.078740;
+                this.mi = input / 63_360;
+                this.nm = input / 72_913.385827;
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                return false;
+            }
         }
 
-        public Scale build() throws InsufficientParameterException {
+        private boolean convertAllFromFT(double input) {
+            try {
+                this.um = input * 304_800;
+                this.mm = input * 304.8;
+                this.cm = input * 30.48;
+                this.in = input  * 12;
+                this.ft = input;
+                this.yd = input / 3;
+                this.m = input / 3.280840;
+                this.ftm = input / 6;
+                this.km = input / 3_280.839895;
+                this.mi = input / 5_280;
+                this.nm = input / 6_080;
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                return false;
+            }
+        }
+
+        private boolean convertAllFromYD(double input) {
+            try {
+                this.um = input * 914_400;
+                this.mm = input * 914.4;
+                this.cm = input * 91.44;
+                this.in = input * 36;
+                this.ft = input * 3;
+                this.yd = input;
+                this.m = input / 1.093613;
+                this.ftm = input / 2;
+                this.km = input / 1_093.613298;
+                this.mi = input / 1_760;
+                this.nm = input / 2025.371829;
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                return false;
+            }
+        }
+
+        private boolean convertAllFromM(double input) {
+            try {
+                this.um = input * 1_000_000;
+                this.mm = input * 1_000;
+                this.cm = input * 100;
+                this.in = input * 39.370079;
+                this.ft = input * 3.28084;
+                this.yd = input * 1.093613;
+                this.m = input;
+                this.ftm = input / 1.828800;
+                this.km = input / 1_000;
+                this.mi = input / 1609.344;
+                this.nm = input / 1852;
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                return false;
+            }
+        }
+
+        private boolean convertAllFromFTM(double input) {
+            try {
+                this.um = input * 1_828_800;
+                this.mm = input * 1_828.8;
+                this.cm = input * 182.88;
+                this.in = input * 72;
+                this.ft = input * 6;
+                this.yd = input * 2;
+                this.m = input * 1.8288;
+                this.ftm = input;
+                this.km = input / 546.806649;
+                this.mi = input / 880;
+                this.nm = input / 1012.685914;
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                return false;
+            }
+        }
+
+        private boolean convertAllFromKM(double input) {
+            try {
+                this.um = input * 1_000_000_000;
+                this.mm = input * 1_000_000;
+                this.cm = input * 100_000;
+                this.in = input * 39_370.078740;
+                this.ft = input * 3_280.839895;
+                this.yd = input * 1_093.613298;
+                this.m = input * 1_000;
+                this.ftm = input * 546.806649;
+                this.km = input;
+                this.mi = input / 1.609344;
+                this.nm = input / 1.852;
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                return false;
+            }
+        }
+
+        private boolean convertAllFromMI(double input) {
+            try {
+                this.um = input * 1_609_344_000;
+                this.mm = input * 1_609_344;
+                this.cm = input * 160_934.4;
+                this.in = input * 63_360;
+                this.ft = input * 5_280;
+                this.yd = input * 1_760;
+                this.m = input * 1_609.344;
+                this.ftm = input * 880;
+                this.km = input * 1.609344;
+                this.mi = input;
+                this.nm = input / 1.150779;
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                return false;
+            }
+        }
+
+        private boolean convertAllFromNM(double input) {
+            try {
+                this.um = input * 1_852_000_000;
+                this.mm = input * 1_852_000;
+                this.cm = input * 185_200;
+                this.in = input * 72_913.385827;
+                this.ft = input * 6_076.115486;
+                this.yd = input * 2_025.371829;
+                this.m = input * 1_852;
+                this.ftm = input * 1_012.685914;
+                this.km = input * 1.852;
+                this.mi = input * 1.150779;
+                this.nm = input;
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                return false;
+            }
+        }
+
+
+        public LengthEntry build() throws InsufficientParameterException {
             System.out.println(this);
-            Scale scale = new Scale(this);
-            if(scale.fractional == 0
-                    || scale.inToMile == 0
-                    || scale.mileToIn == 0
-                    || scale.cmToKm == 0
-                    || scale.kmToCm == 0) {
+            LengthEntry lengthEntry = new LengthEntry(this);
+            if(lengthEntry.um == 0
+                    || lengthEntry.mm == 0
+                    || lengthEntry.cm == 0
+                    || lengthEntry.in == 0
+                    || lengthEntry.ft == 0
+                    || lengthEntry.yd == 0
+                    || lengthEntry.m == 0
+                    || lengthEntry.ftm == 0
+                    || lengthEntry.km == 0
+                    || lengthEntry.mi == 0
+                    || lengthEntry.nm == 0) {
                 String message = "There has to be at least one parawmeter set in the builder";
                 throw new InsufficientParameterException(message);
             }
-            return scale;
+            return lengthEntry;
         }
+
+    }
+
+
 
 }
